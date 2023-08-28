c5be8da183a92c7ec4611897e67be234987aa005
Vaibhav <shendagevaibhav26@gmail.com>
2023-08-28 15:49:10 +0530

    Properties file Added

--- src/main/java/com/rt/SpringBootProjectApplication.java
+++ src/main/java/com/rt/SpringBootProjectApplication.java
@@ -2,7 +2,8 @@ package com.rt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.PropertySource;
+@PropertySource(value = "classpath:Student_properties")
 @SpringBootApplication
 public class SpringBootProjectApplication {
 
